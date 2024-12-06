9cf3a9273924a07207c3ad14f738bf72e314dadb
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2024-12-06 23:24:15 +0900

    Modify the output so that no warnings appear at the beginning.

--- sau3.java
+++ sau3.java
@@ -1,8 +1,30 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS com.github.oogasawa:Utility-sau3:1.6.0
+
+import java.io.File;
+import java.net.URLClassLoader;
 
 class sau3 {
+
+  static String fatJar = "/works/Utility-sau3/target/Utility-sau3-1.6.1-fat.jar";
+
   public static void main(String[] args) throws Exception {
-    com.github.oogasawa.utility.sau3.App.main(args);
+    // Get the user's home directory from Java system properties
+    String homeDir = System.getProperty("user.home");
+    File jarFile = new File(homeDir + fatJar);
+
+    if (!jarFile.exists()) {
+      throw new RuntimeException("JAR file not found: " + jarFile.getAbsolutePath());
+    }
+
+    // Dynamically add the JAR file to the classpath
+    URLClassLoader classLoader = new URLClassLoader(
+        new java.net.URL[] { jarFile.toURI().toURL() },
+        sau3.class.getClassLoader()
+    );
+
+    // Load the class and invoke the main method
+    Class<?> appClass = classLoader.loadClass("com.github.oogasawa.utility.sau3.App");
+    appClass.getMethod("main", String[].class).invoke(null, (Object) args);
   }
 }
+
